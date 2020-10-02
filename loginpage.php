7fbe9903c42f145f95ee34027b91ba96f0eed950
darshangawade <59640948+darshangawade@users.noreply.github.com>
2020-10-02 12:08:16 +0530

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -1,9 +1,8 @@
 <html>
 <head>
-<title>MedTech Solution</title>
+<title>SIH2020</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Tab logo -->
-<link href="assets/img/tab_logo.png" rel="icon">
+
 <!-- main js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="javascript/main.js"></script>
